70f1dbb7547d221887c51c7c08724ec6f7aac7a6
amritansh-raj <amritansh.raj21@gmail.com>
2023-07-18 18:09:44 +0530

    toggle div added

--- script.js
+++ script.js
@@ -1,32 +1,34 @@
 var myApp = angular.module("myApp", ["ui.router"]);
 userLoggedIn = false;
+var apiUrl = "http://10.21.82.46:8000";
 
 myApp.config(function ($stateProvider, $urlRouterProvider) {
   $urlRouterProvider.otherwise("/Home");
 
-  $stateProvider.state("Home", {
-    url: "/Home",
-    templateUrl: "index.html",
-    controller: "indexController",
-  })
-  .state("Products", {
-    url: "/Products",
-    templateUrl: "template/products.html",
-    controller: "productController",
-  })
-  .state("category", {
-    url: "/Category",
-    templateUrl: "template/category.html",
-    controller: "categoryController",
-  })
-  .state("manager", {
-    url: "/manager",
-    templateUrl: "template/manager.html",
-    controller: "managerController",
-  });
+  $stateProvider
+    .state("Home", {
+      url: "/Home",
+      templateUrl: "index.html",
+      controller: "indexController",
+    })
+    .state("Products", {
+      url: "/Products",
+      templateUrl: "template/products.html",
+      controller: "productController",
+    })
+    .state("category", {
+      url: "/Category",
+      templateUrl: "template/category.html",
+      controller: "categoryController",
+    })
+    .state("manager", {
+      url: "/manager",
+      templateUrl: "template/manager.html",
+      controller: "managerController",
+    });
 });
 
-myApp.controller("indexController", function ($scope) {
+myApp.controller("indexController", function ($scope, $http) {
   $scope.showLoginPopup = function () {
     Swal.fire({
       title: `<p class="text-center h1 fw-bold mb-3 mx-1 mx-md-4 mt-4">
@@ -65,8 +67,21 @@ myApp.controller("indexController", function ($scope) {
           return false;
         }
 
-        console.log("Username:", username);
-        console.log("Password:", password);
+        var loginData = {
+          username: username,
+          password: password,
+        };
+
+        console.log(loginData);
+
+        $http
+          .post(apiUrl + "/shopify/login", loginData)
+          .then(function (response) {
+            console.log(response);
+          })
+          .catch(function (error) {
+            console.log("error", error);
+          });
       },
       allowOutsideClick: function () {
         return !Swal.isLoading();
@@ -87,8 +102,16 @@ myApp.controller("indexController", function ($scope) {
             <label >Username</label>
         </div>
         <div class="form-floating mb-3">
-            <input type="email" id="email" class="form-control" placeholder="email@example.com" required>
+            <input type="email" id="email" class="form-control" placeholder="email@example.com">
             <label >Email</label>
+        </div>
+        <div class="form-floating mb-3">
+            <input type="tel" id="contact" class="form-control" placeholder="Phone no">
+            <label >Phone no</label>
+        </div>
+        <div class="form-floating mb-3">
+            <input type="text" id="address" class="form-control" placeholder="Address">
+            <label >Address</label>
         </div>
         <div class="form-floating mb-3">
             <input type="password" id="password" class="form-control" placeholder="Password">
@@ -106,19 +129,31 @@ myApp.controller("indexController", function ($scope) {
       preConfirm: function () {
         var username = Swal.getPopup().querySelector("#username").value;
         var email = Swal.getPopup().querySelector("#email").value;
+        var contact = Swal.getPopup().querySelector("#contact").value;
+        var address = Swal.getPopup().querySelector("#address").value;
         var password = Swal.getPopup().querySelector("#password").value;
         var confirmPassword =
           Swal.getPopup().querySelector("#confirmPassword").value;
 
-        if (!username || !email || !password || !confirmPassword) {
+        if (
+          !username ||
+          !email ||
+          !password ||
+          !confirmPassword ||
+          !contact ||
+          !address
+        ) {
           Swal.showValidationMessage("Please fill all the fields");
           return false;
         }
 
-        var emailField = Swal.getPopup().querySelector("#email");
-
         if (!validateEmail(email)) {
           Swal.showValidationMessage("Invalid email address");
+          return false;
+        }
+
+        if (!validateContact(contact)) {
+          Swal.showValidationMessage("Invalid Contact");
           return false;
         }
 
@@ -127,10 +162,24 @@ myApp.controller("indexController", function ($scope) {
           return false;
         }
 
-        console.log("Username:", username);
-        console.log("Email:", email);
-        console.log("Password:", password);
-        console.log("Confirm Password:", confirmPassword);
+        var registerData = {
+          username: username,
+          email: email,
+          contact: contact,
+          address: address,
+          password: password,
+          confirmPassword: confirmPassword,
+        };
+
+        $http
+          .post(apiUrl + "/shopify/register", registerData)
+          .then(function (response) {
+            console.log(response);
+          })
+          .catch(function (error) {
+            console.log("error", error);
+          });
+
       },
       allowOutsideClick: function () {
         return !Swal.isLoading();
@@ -146,6 +195,11 @@ myApp.controller("indexController", function ($scope) {
     return emailPattern.test(email);
   }
 
+  function validateContact(contact) {
+    var contactPattern = /\s*[1-9]{1}[0-9]{9}\s*/;
+    return contactPattern.test(contact);
+  }
+
   $(document).on("click", "#addItemBtn", function (event) {
     if (!userLoggedIn) {
       event.preventDefault();
@@ -155,6 +209,10 @@ myApp.controller("indexController", function ($scope) {
   });
 });
 
-myApp.controller("managerController", function($scope){
-    
-})
+myApp.controller("managerController", function ($scope) {
+  $scope.panels = [{ showDropdown: false }, { showDropdown: false }];
+
+  $scope.toggleDropdown = function (panel) {
+    panel.showDropdown = !panel.showDropdown;
+  };
+});
