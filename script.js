8831499fb139363bb6d5daa5c0758c1cbb8bafe2
amritansh-raj <amritansh.raj21@gmail.com>
2023-08-13 17:35:40 +0530

    image working

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 var myApp = angular.module("myApp", ["ui.router", "ngCookies"]);
 var apiUrl = "https://10.21.82.46:8000/shopify/";
-// var apiUrl = "http://10.21.83.42:8000/"
 
 myApp.config(function ($stateProvider, $urlRouterProvider) {
   $urlRouterProvider.otherwise("/Home");
@@ -38,59 +37,65 @@ myApp.config(function ($stateProvider, $urlRouterProvider) {
     });
 });
 
-myApp.factory("alertService", ["$rootScope", "$timeout", function($rootScope, $timeout) {
+myApp.factory("alertService", [
+  "$rootScope",
+  "$timeout",
+  function ($rootScope, $timeout) {
+    var alertService = {
+      add: function (type, msg, section) {
+        if (section === undefined) section = 1;
+        $rootScope.alerts.push({
+          type: type,
+          msg: msg,
+          section: section,
+          close: function () {
+            return alertService.closeAlert(this);
+          },
+        });
+      },
 
-  var alertService = {
-    add: function(type, msg, section) {
-      if (section === undefined) section = 1;
-      $rootScope.alerts.push({
-        'type': type,
-        'msg': msg,
-        'section': section,
-        close: function() {
-          return alertService.closeAlert(this);
-        }
-      });
-    },
+      closeAlert: function (alert) {
+        this.closeAlertIdx($rootScope.alerts.indexOf(alert));
+      },
+      closeAlertIdx: function (index) {
+        return $rootScope.alerts.splice(index, 1);
+      },
+      clear: function () {
+        $rootScope.alerts = [];
+      },
+    };
 
-    closeAlert: function(alert) {
-      this.closeAlertIdx($rootScope.alerts.indexOf(alert));
-    },
-    closeAlertIdx: function(index) {
-      return $rootScope.alerts.splice(index, 1);
-    },
-    clear: function() {
-      $rootScope.alerts = [];
-    }
-  };
+    $rootScope.$on("$stateChangeSuccess", function () {
+      alertService.clear();
+    });
 
-  $rootScope.$on('$stateChangeSuccess', function() {
-    alertService.clear();
-  });
+    $rootScope.alerts = [];
 
-  $rootScope.alerts = [];
+    return alertService;
+  },
+]);
 
-  return alertService;
-}]);
+myApp.controller("indexController", [
+  "$scope",
+  "$http",
+  "$state",
+  function ($scope, $http, $state) {
+    $scope.userLoggedIn = false;
 
-
-myApp.controller("indexController", function ($scope, $http, $state, $cookies) {
-  $scope.userLoggedIn = false;
-
-  $scope.showLoginPopup = function () {
-    Swal.fire({
-      title: `<p class="text-center h1 fw-bold mb-3 mx-1 mx-md-4 mt-4">
+    $scope.showLoginPopup = function () {
+      Swal.fire({
+        title: `<p class="text-center h1 fw-bold mb-3 mx-1 mx-md-4 mt-4">
       Login or Register
     </p>`,
-      html: `
+        html: `
       <div class="form-floating mb-3">
         <input
-          type="email"
+          type="text"
           class="form-control"
           id="username"
           placeholder="Username"
         />
-        <label >Email</label>
+        <label >Username</label>
       </div>
       <div class="form-floating mb-3">
         <input
@@ -102,89 +107,86 @@ myApp.controller("indexController", function ($scope, $http, $state, $cookies) {
         <label>Password</label>
       </div>
           `,
-      showCancelButton: true,
-      confirmButtonText: "Login",
-      cancelButtonText: "Register",
-      showLoaderOnConfirm: true,
-      preConfirm: function () {
-        var username = Swal.getPopup().querySelector("#username").value;
-        var password = Swal.getPopup().querySelector("#password").value;
+        showCancelButton: true,
+        confirmButtonText: "Login",
+        cancelButtonText: "Register",
+        showLoaderOnConfirm: true,
+        preConfirm: function () {
+          var username = Swal.getPopup().querySelector("#username").value;
+          var password = Swal.getPopup().querySelector("#password").value;
 
-        if (!username || !password) {
-          Swal.showValidationMessage("Please enter both username and password");
-          return false;
-        }
+          if (!username || !password) {
+            Swal.showValidationMessage(
+              "Please enter both username and password"
+            );
+            return false;
+          }
 
-        var loginData = {
-          username: username,
-          password: password,
-        };
+          var loginData = {
+            username: username,
+            password: password,
+          };
 
-        console.log(loginData);
-
-        $http({
-          method: "POST",
-          url: apiUrl + "login/",
-          // url: apiUrl + "custom_login/",
-          data: loginData,
-          withCredentials: true
-        })
-          .then(function (response) {
-
-            $scope.userLoggedIn = true;  
-  
-            var verify = response.data.is_superuser;
-            superuser = verify;
-
-            console.log(response);
-            if (superuser) {
-              $state.go("manager");
-
-            } else {
-              $state.go("Home");
-            }
+          $http({
+            method: "POST",
+            url: apiUrl + "login/",
+            data: loginData,
+            withCredentials: true,
           })
-          .catch(function (error) {
-            console.log("error", error);
-          });
-      },
-      allowOutsideClick: function () {
-        return !Swal.isLoading();
-      },
-    }).then(function (result) {
-      if (result.dismiss === Swal.DismissReason.cancel) {
-        $scope.showRegisterPopup();
-      }
-    });
+            .then(function (response) {
+              $scope.userLoggedIn = true;
 
-    $(document).on("click", "#addItemBtn", function (event) {
-      event.preventDefault();
+              var verify = response.data.is_superuser;
+              superuser = verify;
 
-      $scope.showLoginPopup();
-    });
-  };
-
-  $scope.logout = function () {
-    $scope.userLoggedIn = false;
-
-    $http({
-      method: "GET",
-      url: apiUrl + "logout/",
-      withCredentials: true,
-    })
-      .then(function (response) {
-        console.log(response);
-      })
-      .catch(function (error) {
-        console.log(error);
+              console.log(response);
+              if (superuser) {
+                $state.go("manager");
+              } else {
+                $state.go("Home");
+              }
+            })
+            .catch(function (error) {
+              console.log("error", error);
+            });
+        },
+        allowOutsideClick: function () {
+          return !Swal.isLoading();
+        },
+      }).then(function (result) {
+        if (result.dismiss === Swal.DismissReason.cancel) {
+          $scope.showRegisterPopup();
+        }
       });
-    $state.go("Home");
-  };
 
-  $scope.showRegisterPopup = function () {
-    Swal.fire({
-      title: "Register",
-      html: `
+      $(document).on("click", "#addItemBtn", function (event) {
+        event.preventDefault();
+
+        $scope.showLoginPopup();
+      });
+    };
+
+    $scope.logout = function () {
+      $scope.userLoggedIn = false;
+
+      $http({
+        method: "GET",
+        url: apiUrl + "logout/",
+        withCredentials: true,
+      })
+        .then(function (response) {
+          console.log(response);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+      $state.go("Home");
+    };
+
+    $scope.showRegisterPopup = function () {
+      Swal.fire({
+        title: "Register",
+        html: `
         <div class="form-floating mb-3">
             <input type="text" id="username" class="form-control" placeholder="Username">
             <label >Username</label>
@@ -210,162 +212,194 @@ myApp.controller("indexController", function ($scope, $http, $state, $cookies) {
             <label >Confirm Password</label>
         </div>
           `,
-      showCancelButton: true,
-      confirmButtonText: "Register",
-      cancelButtonText: "Cancel",
-      showLoaderOnConfirm: true,
-      preConfirm: function () {
-        var username = Swal.getPopup().querySelector("#username").value;
-        var email = Swal.getPopup().querySelector("#email").value;
-        var contact = Swal.getPopup().querySelector("#contact").value;
-        var address = Swal.getPopup().querySelector("#address").value;
-        var password = Swal.getPopup().querySelector("#password").value;
-        var confirmPassword =
-          Swal.getPopup().querySelector("#confirmPassword").value;
+        showCancelButton: true,
+        confirmButtonText: "Register",
+        cancelButtonText: "Cancel",
+        showLoaderOnConfirm: true,
+        preConfirm: function () {
+          var username = Swal.getPopup().querySelector("#username").value;
+          var email = Swal.getPopup().querySelector("#email").value;
+          var contact = Swal.getPopup().querySelector("#contact").value;
+          var address = Swal.getPopup().querySelector("#address").value;
+          var password = Swal.getPopup().querySelector("#password").value;
+          var confirmPassword =
+            Swal.getPopup().querySelector("#confirmPassword").value;
 
-        if (
-          !username ||
-          !email ||
-          !password ||
-          !confirmPassword ||
-          !contact ||
-          !address
-        ) {
-          Swal.showValidationMessage("Please fill all the fields");
-          return;
-        }
+          if (
+            !username ||
+            !email ||
+            !password ||
+            !confirmPassword ||
+            !contact ||
+            !address
+          ) {
+            Swal.showValidationMessage("Please fill all the fields");
+            return;
+          }
 
-        if (!validateEmail(email)) {
-          Swal.showValidationMessage("Invalid email address");
-          return;
-        }
+          if (!validateEmail(email)) {
+            Swal.showValidationMessage("Invalid email address");
+            return;
+          }
 
-        if (!validateContact(contact)) {
-          Swal.showValidationMessage("Invalid Contact");
-          return;
-        }
+          if (!validateContact(contact)) {
+            Swal.showValidationMessage("Invalid Contact");
+            return;
+          }
 
-        if (password !== confirmPassword) {
-          Swal.showValidationMessage("Password does not match");
-          return;
-        }
+          if (password !== confirmPassword) {
+            Swal.showValidationMessage("Password does not match");
+            return;
+          }
 
-        var registerData = {
-          username: username,
-          email: email,
-          contact: contact,
-          address: address,
-          password: password,
-          confirmPassword: confirmPassword,
-        };
+          var registerData = {
+            username: username,
+            email: email,
+            contact: contact,
+            address: address,
+            password: password,
+            confirmPassword: confirmPassword,
+          };
 
-        $http({
-          method: "POST",
-          url: apiUrl + "register/",
-          data: registerData,
-        })
-          .then(function (response) {
-            userLoggedIn = true;
-            console.log(response);
+          $http({
+            method: "POST",
+            url: apiUrl + "register/",
+            data: registerData,
           })
-          .catch(function (error) {
-            console.log("error", error);
-          });
-      },
-      allowOutsideClick: function () {
-        return !Swal.isLoading();
-      },
-    }).then(function (result) {
-      if (result.dismiss === Swal.DismissReason.cancel) {
-      }
+            .then(function (response) {
+              userLoggedIn = true;
+              console.log(response);
+            })
+            .catch(function (error) {
+              console.log("error", error);
+            });
+        },
+        allowOutsideClick: function () {
+          return !Swal.isLoading();
+        },
+      }).then(function (result) {
+        if (result.dismiss === Swal.DismissReason.cancel) {
+        }
+      });
+    };
+
+    function validateEmail(email) {
+      var emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+      return emailPattern.test(email);
+    }
+
+    function validateContact(contact) {
+      var contactPattern = /\s*[1-9]{1}[0-9]{9}\s*/;
+      return contactPattern.test(contact);
+    }
+
+    $(document).on("click", "#loginBtn", function (event) {
+      event.preventDefault();
+
+      $scope.showLoginPopup();
     });
-  };
+  },
+]);
 
-  function validateEmail(email) {
-    var emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-    return emailPattern.test(email);
-  }
+myApp.controller("managerController", [
+  "$scope",
+  "$http",
+  "$state",
+  "alertService",
+  function ($scope, $http, $state, alertService) {
+    alertService.add("success", "Welcome to your dashboard", 1);
 
-  function validateContact(contact) {
-    var contactPattern = /\s*[1-9]{1}[0-9]{9}\s*/;
-    return contactPattern.test(contact);
-  }
+    $scope.categories = [];
 
-  $(document).on("click", "#loginBtn", function (event) {
-    event.preventDefault();
+    $http({
+      method: "GET",
+      url: apiUrl + "getcategory/",
+      withCredentials: true,
+    })
+      .then(function (response) {
+        console.log(response);
+        var categories = response.data;
 
-    $scope.showLoginPopup();
-  });
-});
+        if (categories) {
+          $scope.categories = categories;
+        }
 
-myApp.controller("managerController", [ "$scope", "$http", "$cookies", "$state", 'alertService', function ($scope, $http, $cookies, $state, alertService) {
-  
-  addService.add('success', 'Welcome to your dashboard', 1);
-  
-  $scope.showCreateSection = function (event) {
-    event.preventDefault();
+        console.log($scope.categories);
 
-    Swal.fire({
-      title: "Create Section",
-      html: `<div class="input-group">
+        // $scope.displayCategories();
+      })
+      .catch(function (error) {
+        if (error.data && error.data.message) {
+          $window.alert(error.data.message);
+        } else {
+          $window.alert("An error occured. Please try again");
+        }
+      });
+
+    $scope.showCreateSection = function (event) {
+      event.preventDefault();
+
+      Swal.fire({
+        title: "Create Section",
+        html: `<div class="input-group">
     <div class="custom-file">
       <input type="text" class="section-name" id="sectionName">
       <input type="file" class="custom-file-input" id="sectionImage" accept="image/*">
     </div>
   </div>`,
-      confirmButtonText: "Create",
-      showCancelButton: true,
-      cancelButtonText: "Cancel",
-    }).then(function (result) {
-      if (result.isConfirmed) {
-        var sectionName = document.getElementById("sectionName").value;
-        var sectionImage = document.getElementById("sectionImage").files[0];
+        confirmButtonText: "Create",
+        showCancelButton: true,
+        cancelButtonText: "Cancel",
+      }).then(function (result) {
+        if (result.isConfirmed) {
+          var sectionName = document.getElementById("sectionName").value;
+          var sectionImage = document.getElementById("sectionImage").files[0];
 
-        var formData = new FormData();
-        formData.append("categoty_name", sectionName)
-        formData.append("category_image", sectionImage);
+          var formData = new FormData();
+          formData.append("category_name", sectionName);
+          formData.append("category_image", sectionImage);
 
-        console.log(formData);
+          console.log(formData);
 
-        $http({
-          method: "POST",
-          // url: apiUrl + "add_section/",
-          url: apiUrl + "addcategory/",
-          withCredentials: true,
-          data: formData,
-          headers: { "Content-Type": undefined },
-        })
-          .then(function (response) {
-            console.log(response)
-            if (response.data.authenticate_id) {
-              console.log("User is authenticated");
-            } else {
-              console.log("User is not authenticated");
-            }
+          $http({
+            method: "POST",
+            url: apiUrl + "addcategory/",
+            withCredentials: true,
+            data: formData,
+            headers: { "Content-Type": undefined },
           })
-          .catch(function (error) {
-            console.error(error);
-          });
-      }
-    });
-  };
-
-  $scope.logout = function () {
-    $scope.userLoggedIn = false;
-
-    $http({
-      method: "GET",
-      url: apiUrl + "logout/",
-    })
-      .then(function (response) {
-        console.log(response);
-      })
-      .catch(function (error) {
-        console.log(error);
+            .then(function (response) {
+              console.log(response);
+              // if (response.data.authenticate_id) {
+              //   console.log("User is authenticated");
+              // } else {
+              //   console.log("User is not authenticated");
+              // }
+            })
+            .catch(function (error) {
+              console.error(error);
+            });
+        }
       });
-    $state.go("Home");
-  }
-}]);
+    };
+
+    $scope.logout = function () {
+      $scope.userLoggedIn = false;
+
+      $http({
+        method: "GET",
+        url: apiUrl + "logout/",
+      })
+        .then(function (response) {
+          console.log(response);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+      $state.go("Home");
+    };
+  },
+]);
 
 myApp.controller("cartController", function ($scope) {});
 
